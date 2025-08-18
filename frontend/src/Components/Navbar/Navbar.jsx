7c4a60164bd1f3e66bce97a93b7bfb2bb882ecd2
Senarathna.K.H <kaveeshaheshan590@gmail.com>
2025-08-18 17:58:56 +0530

    Navbar create

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import './Navbar.css'
-import logo from '../Asset/logo.png'
+import logot from '../Asset/logot.png'
 import cart_icon from '../Asset/cart_icon.png'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="nav-log">
-            <img src={logo} alt=""/>
-            <p>SHOPPER</p>
+            <img src={logot } alt=""/>
+            <p>JOSEPA</p>
 
         </div>
+        <ui className="nav-menu">
+          <li >Shop</li>
+          <li >Men</li>
+          <li >Women</li>
+          <li >Kids</li>
+          
+        </ui>
+        <div className="nav login cart"></div>
+        <button>login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav cart count">0</div>
     </div>
   )
 }
