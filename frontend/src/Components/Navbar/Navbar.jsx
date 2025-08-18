fdbbb64adb5a20b9e8c4cd561814ced9f5c37506
Senarathna.K.H <kaveeshaheshan590@gmail.com>
2025-08-18 18:45:21 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'   // added useState import
 import './Navbar.css'
 import logot from '../Asset/logot.png'
 import cart_icon from '../Asset/cart_icon.png'
 
 const Navbar = () => {
+  const [menu,setmenu] = useState("shop");
   return (
     <div className='navbar'>
         <div className="nav-log">
             <img src={logot } alt=""/>
             <p>JOSEPA</p>
-
         </div>
+
         <ui className="nav-menu">
-          <li >Shop</li>
-          <li >Men</li>
-          <li >Women</li>
-          <li >Kids</li>
-          
+          <li onClick={()=>{setmenu("shop")}}>Shop{menu=="shop"?<h/>:<></>}</li>
+          <li onClick={()=>{setmenu("mens")}}>Men{menu=="mens"?<h/>:<></>}</li>
+          <li onClick={()=>{setmenu("womens")}}>Women{menu=="womens"?<h/>:<></>}</li>  {/* fixed: quotes */}
+          <li onClick={()=>{setmenu("kids")}}>Kids{menu=="kids"?<h/>:<></>}</li>      {/* fixed: quotes */}
         </ui>
+
         <div className="nav login cart"></div>
         <button>login</button>
         <img src={cart_icon} alt="" />
