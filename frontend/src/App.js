96cce370887d096f2d5e6c0977693142d7329584
Senarathna.K.H <kaveeshaheshan590@gmail.com>
2025-08-24 22:11:53 +0530

    Update App.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'; // frist import 
@@ -12,18 +11,17 @@ function App() {
   return (
     <div >
       <BrowserRouter>
-     <Navbar/>
-     <Routes>
-      <Route path='/' element={<Shop/>} />
-       <Route path='/' element={<Product/>} />
-        <Route path='/mens' element={<ShopCategory category="mens"/>} />
-         <Route path='/Womens' element={<ShopCategory category="womens"/>} />
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/product' element={<Product/>} />
+          <Route path='/mens' element={<ShopCategory category="mens"/>} />
+          <Route path='/Womens' element={<ShopCategory category="womens"/>} />
           <Route path='/Kids' element={<ShopCategory category="kids"/>} />
-        <Route path='/' element={<Cart/>} />
-         <Route path='/' element={<LoginSigunp/>} />
-      
-     </Routes>
-
+          <Route path=":productId" element={<Product />} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/' element={<LoginSigunp/>} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
